0434581ee3f0f7f5fae171313118d8cd144e3c8f
joceesagar <joshisagarm2@gmail.com>
2025-01-25 13:18:54 +0545

    successfully creadted image upload logic

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default async function Home() {
   const user = await currentUser();
   const posts = await getPosts();
   const dbUserId = await getDbUserId()
-  console.log(posts)
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6 mt-10">
       <div className="lg:col-span-6">
